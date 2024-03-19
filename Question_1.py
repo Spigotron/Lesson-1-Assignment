9112da6b88d5c79a58d0f5b84ac86ffb1d8f7e1b
Spigotron <82724494+Spigotron@users.noreply.github.com>
2024-03-18 20:14:16 -0500

    Question 1

---
+++ Question_1.py
@@ -0,0 +1,14 @@
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+mood = "happy"
+
+print("How do you feel today?")
+if mood == "happy":
+    print("That's great to hear!")
+else:
+    print("I hope your day gets better!")
