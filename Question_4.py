cb2b686fec94a0958b92fbf96504571f280ecf10
Spigotron <82724494+Spigotron@users.noreply.github.com>
2024-03-18 20:46:05 -0500

    Add files via upload

---
+++ Question_4.py
@@ -0,0 +1,14 @@
+# Task 1
+
+bread = 5
+peanut_butter = 3
+jelly = 7
+cost_of_pbj = bread + peanut_butter + jelly
+print(cost_of_pbj)
+
+# Task 2
+
+savings = 1000
+interest_rate = 0.10
+total_amount = savings + savings * interest_rate
+print(total_amount)
